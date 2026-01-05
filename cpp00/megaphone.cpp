fb514b5230bb57587de530556e4c575defea798a
AdnilsonPinheiro <adpmaia@gmail.com>
2026-01-05 19:16:10 +0000

    firts cpp exercise

---
+++ cpp00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int	main(int argc, char **argv){
+	if (argc != 1){
+		std::string	line = "";
+		for (int i = 1; i < argc; i++){
+			line += argv[i];
+			line += " ";
+		}
+		for (size_t i = 0; i < line.length(); i++){
+			line[i] = std::toupper(line[i]);
+		}
+		std::cout << line << std::endl;
+	}
+	else{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+}
