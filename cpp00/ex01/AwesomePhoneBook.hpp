bde3a2d83ec814823e56c1da629254f6da5e2f96
AdnilsonPinheiro <adpmaia@gmail.com>
2026-01-06 18:39:19 +0000

    beginning cpp00/ex01

---
+++ cpp00/ex01/AwesomePhoneBook.hpp
@@ -0,0 +1,9 @@
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include "PhoneBook.hpp"
+# include "Contact.hpp"
+# include <iostream>
+# include <ctime>
+
+#endif
---
+++ cpp00/ex01/PhoneBook.hpp
@@ -0,0 +1,9 @@
+#include "AwesomePhoneBook.hpp"
+
+class PhoneBook{
+	private:
+		Contact	contatos[8];
+	public:
+		add(Contact newContact);
+		search();
+};
