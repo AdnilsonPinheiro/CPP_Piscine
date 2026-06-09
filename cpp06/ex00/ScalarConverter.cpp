53970535b06aee7bf2402dd7b9436d2920a7ec3a
Adnilson Pinheiro <adpmaia@gmail.com>
2026-06-09 07:42:14 +0100

    prototyping

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,2 +1,38 @@
 #include "ScalarConverter.hpp"
+#include <cstdlib>
 
+static void impossible()
+{
+    std::cout << "impossible" << std::endl;
+}
+
+static void printChar(std::string& input)
+{
+    char c = input [0];
+    if (input.length() == 1 && (c > 32 && c < 127))
+        std::cout << input << std::endl;
+    else
+        impossible();
+}
+
+static void printInt(std::string& input)
+{
+    if (input == "nan" || input == "nanf" || input == "+inf" || input == "-inf" || input == "+inff" || input == "-inff")
+        return (impossible());
+    int nbr = atoi(input.c_str());
+}
+
+static void printFloat(std::string& input)
+{
+
+}
+
+static void printDouble(std::string& input)
+{
+
+}
+
+void ScalarConverter::convert(std::string& input)
+{
+
+}
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -2,16 +2,13 @@
 # define SCALARCONVERTER_HPP
 
 # include <string>
-# include <sstream>
 
 class ScalarConverter
 {
 public:
 	static void convert(std::string& input);
 private:
-	std::string data;
 	ScalarConverter();
-	ScalarConverter(const std::string& input);
 	ScalarConverter(const ScalarConverter& src);
 	ScalarConverter& operator=(const ScalarConverter& src);
 	~ScalarConverter();
