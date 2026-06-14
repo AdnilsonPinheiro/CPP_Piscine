a7e0c9fb38e3378ab97cf9de159a544ed8a5124d
Adnilson Pinheiro <adpmaia@gmail.com>
2026-06-14 17:48:52 +0100

    printing from char

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,20 +1,41 @@
 #include "ScalarConverter.hpp"
 
-static int isPseudo(const std::string& str)
-{
-    if (str == "nan" || str == "nanf" || str == "inf" || str == "+inf" || str == "+inff" \
-    || str == "-inf" || str == "-inff")
-        return true;
-    return false;
-}
+static int inputType(const std::string& str);
+static void printFromChar(const char c);
+static void printFromPseudo(const std::string &str);
 
+void ScalarConverter::convert(const std::string& input)
+{
+    int t = inputType(input);
+    switch(t)
+    {
+        case CHAR:
+            printFromChar(input[0]);
+            break;
+        // case INT:
+        //     printFromInt(input);
+        //     break;
+        // case FLOAT:
+        //     printFromFloat(input);
+        //     break;
+        // case DOUBLE:
+        //     printFromDouble(input);
+        //     break;
+        case PSEUDO:
+            printFromPseudo(input);
+            break;
+        case ERROR:
+            std::cerr << "Invalid argument. Must represent a C++ literal" << std::endl;
+    }
+}
 
 static int inputType(const std::string& str)
 {
     int f_count = 0;
     int d_count = 0;
 
-    if (isPseudo(str))
+    if (str == "nan" || str == "nanf" || str == "inf" || str == "inff" || \
+        str == "+inf" || str == "+inff" || str == "-inf" || str == "-inff")
         return PSEUDO;
     if (str.length() == 1 && !isdigit(str[0]))
         return CHAR;
@@ -46,27 +67,25 @@ static int inputType(const std::string& str)
     return INT;
 }
 
-void ScalarConverter::convert(const std::string& input)
+static void printFromChar(const char c)
 {
-    int t = inputType(input);
-    switch(t)
-    {
-        case CHAR:
-            printFromChar(input);
-            break;
-        case INT:
-            printFromInt(input);
-            break;
-        case FLOAT:
-            printFromFloat(input);
-            break;
-        case DOUBLE:
-            printFromDouble(input);
-            break;
-        case PSEUDO:
-            printFromPseudo(input);
-            break;
-        case ERROR:
-            std::cerr << "Invalid argument. Must represent a C++ literal" << std::endl;
-    }
+    if (std::isprint(c))
+        std::cout << "char: '" << c << "'\n";
+    else
+        std::cout << "char: Non displayable\n";
+    std::cout << "int: " << static_cast<int>(c) << "\n";
+    std::cout << std::fixed <<std::setprecision(1);
+    std::cout << "float: " << static_cast<float>(c) << "f\n";
+    std::cout << "double: " << static_cast<double>(c) << std::endl;
+}
+
+static void printFromPseudo(const std::string &str)
+{
+    std::cout << "char: impossible\nint: impossible" << std::endl;
+    if (str == "inf" || str == "inff" || str == "+inf" || str == "+inff")
+        std::cout << "float: +inff\ndouble: +inf" << std::endl;
+    else if (str == "-inf" || str == "-inff")
+        std::cout << "float: -inff\ndouble: -inf" << std::endl;
+    else
+        std::cout << "float: nanf\ndouble: nan" << std::endl;
 }
