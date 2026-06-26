ce8eac21b2f4fcfcd2a748f2c320cf978a86b5ff
AdnilsonPinheiro <adpmaia@gmail.com>
2026-06-26 16:43:06 +0100

    tests done

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -11,5 +11,36 @@ int main()
     ::iter(str1, 5, ft_print<char>);
     std::cout << std::endl;
 
+    std::cout << "===== [NON-CONST CHAR] =====" << std::endl;
+
+    char str2[] = {'n', 'o', 'n', '-', 'c', 'o', 'n', 's', 't'};
+
+    ::iter(str2, sizeof(str2), ft_print<char>);
+    std::cout << std::endl;
+
+    std::cout << "===== [DEREFERENCING NON-CONST CHAR ARRAY] =====" << std::endl;
+    str2[0] = '_';
+    ::iter(str2, sizeof(str2), ft_print<char>);
+    std::cout << std::endl;
+    std::cout << "===== [CONST INT] =====" << std::endl;
+
+    const int array1[] = {0, 1, 2, 3, 4};
+
+    ::iter(array1, sizeof(array1)/sizeof(int), ft_print<int>);
+    std::cout << std::endl;
+
+    std::cout << "===== [NON-CONST INT] =====" << std::endl;
+
+    int array2 [] = {9, 8, 7, 6, 5};
+
+    ::iter(array2, sizeof(array2)/sizeof(int), ft_print<int>);
+    std::cout << std::endl;
+
+    std::cout << "===== [DEFERENCING NON-CONST INT ARRAY] =====" << std::endl;
+    array2[0] = 1;
+    ::iter(array2, sizeof(array2)/sizeof(int), ft_print<int>);
+    std::cout << std::endl;
+
+
     return 0;
 }
